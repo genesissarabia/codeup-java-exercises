6975fc2b9fc6d696323443787c19b13f1e80d64d
Genesis Sarabia <genesissarabia14@gmail.com>
2023-03-28 23:27:43 -0500

    computer issues, still working on factorial exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,31 +3,31 @@ public class MethodsExercises {
 
     //1.
     public static void main(String [] args){
-        System.out.println(add(4, 5));
-        System.out.println(subtract(4, 5));
-        System.out.println(multiply(4, 5));
-        System.out.println(division(20, 5));
-        System.out.println(modulus(20, 5));
+//        System.out.println(add(4, 5));
+//        System.out.println(subtract(4, 5));
+//        System.out.println(multiply(4, 5));
+//        System.out.println(division(20, 5));
+//        System.out.println(modulus(20, 5));
 
         getInteger(1, 10);
 
     }
 
-    public static int add(int x, int y){
-        return x+y;
-    }
-    public static int subtract(int x, int y){
-        return x-y;
-    }
-    public static int multiply(int x, int y){
-        return x*y;
-    }
-    public static int division(int x, int y){
-        return x/y;
-    }
-    public static int modulus(int x, int y){
-        return x%y;
-    }
+//    public static int add(int x, int y){
+//        return x+y;
+//    }
+//    public static int subtract(int x, int y){
+//        return x-y;
+//    }
+//    public static int multiply(int x, int y){
+//        return x*y;
+//    }
+//    public static int division(int x, int y){
+//        return x/y;
+//    }
+//    public static int modulus(int x, int y){
+//        return x%y;
+//    }
 
     //2.
 
@@ -47,16 +47,27 @@ public class MethodsExercises {
     //3.
 
     public static int getInteger(int min, int max){
-        Scanner numberInput = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number between 1 and 10: ");
-        int number = numberInput.nextInt();
+        int number = sc.nextInt();
+        boolean confirm;
 
-        if (max < number || min > number){
-            System.out.println("Not within range, please enter another number.");
-            return getInteger(min, max);
-        }
-        System.out.println("Your number is valid! Your number is: " + number);
-        return number;
+        do {
+            System.out.print("\nPlease enter a number from 0-100");
+            long userInput = sc.nextInt();
+
+            if (max < number || min > number){
+                System.out.println("Not within range, please enter another number.");
+                return getInteger(min, max);
+            } else {
+                System.out.println("Your number is valid! Your number is: " + number);
+                return number;
+            }
+
+            System.out.printf("Do you want to continue? Yes or No?");
+            confirm = sc.next().equalsIgnoreCase("yes");
+
+        } while (confirm);
     }
 
 }
