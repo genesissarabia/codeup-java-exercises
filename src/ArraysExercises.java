760e1b93ba2a3f1cdd7e979d0fe0f0b9f0f335b2
Genesis Sarabia <genesissarabia14@gmail.com>
2023-04-01 16:42:53 -0500

    working on #1 of java arraysExercises

---
+++ src/ArraysExercises.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+        Person genesis = new Person("Genni");
+        Person isleny = new Person("Lenny");
+        Person larissa = new Person("Atzi");
+
+        Person[] threePeople = {genesis, isleny, larissa};
+
+        for(Person onePerson: threePeople){
+            System.out.println(onePerson.getName());
+        }
+
+    }
+
+    public static Person addPerson(String [] args){
+
+        
+
+
+
+    }
+
+
+
+
+
+//    public static String addPerson(String[] args) {
+//
+//        String[] persons = new String[1];
+//
+//        System.out.println(persons.length);
+//
+//    }
+}
