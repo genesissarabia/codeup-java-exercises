0a50ffb9866ecd535c12e0241cd475654e9cb728
Genesis Sarabia <genesissarabia14@gmail.com>
2023-04-03 16:36:52 -0500

    completed Inheritance and Polymorphism exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -8,7 +8,7 @@ public class ShapesTest {
         System.out.println(box1.getPerimeter());
         System.out.println(box1.getArea());
 
-        Rectangle box2 = new Square(5);
+        Square box2 = new Square(5);
         System.out.println(box2.getPerimeter());
         System.out.println(box2.getArea());
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,9 +2,23 @@ package shapes;
 
 public class Square extends Rectangle{
 
+    private double side;
+
     public Square(double side){
         super(side, side);
+        this.side = side;
     }
+
+    @Override
+    public double getArea() {
+        return 4*this.side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return Math.pow(this.side, 2);
+    }
+
 
 
 }
