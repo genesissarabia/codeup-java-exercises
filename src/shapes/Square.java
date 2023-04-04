978a6be438791a2813e4d696a1cfdc92e17ca23a
Genesis Sarabia <genesissarabia14@gmail.com>
2023-04-04 15:15:59 -0500

    corrected square setMethods to add length and width and removed public from Quadrilateral class

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,31 +1,32 @@
 package shapes;
 
-public class Square extends Rectangle{
+public class Square extends Quadrilateral{
 
-    protected double side;
+
     public Square(double side) {
         super(side, side);
-        this.side = side;
     }
 
     @Override
-    public void setWidth(double width) {
-        this.width = width;
+    public void setWidth(double side) {
+        this.width = side;
+        this.length = side;
     }
 
     @Override
     public void setLength(double length) {
         this.length = length;
+        this.width = length;
     }
 
     @Override
     public double getPerimeter() {
-        return super.getPerimeter();
+        return 4*length;
     }
 
     @Override
     public double getArea() {
-        return super.getArea();
+        return Math.pow(length, 2);
     }
 
 
